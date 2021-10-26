89b2a5bc617d283b8b02324522597e7e4a78c7f7
Anders Hald <chald80@gmail.com>
2021-10-26 19:06:39 +0200

    Add project files.

---
+++ Hop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace HopClassLib
+{
+    public class Hop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double AlphaAcid { get; set; }
+        public int HarvestYear { get; set; }
+        public int Price { get; set; }
+
+        public Hop(int id, string name, double alphaAcid, int harvestYear, int price)
+        {
+            Id = id;
+            Name = name;
+            AlphaAcid = alphaAcid;
+            HarvestYear = harvestYear;
+            Price = price;
+        }
+
+        public Hop()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}, {nameof(AlphaAcid)}: {AlphaAcid}, {nameof(HarvestYear)}: {HarvestYear}, {nameof(Price)}: {Price}";
+        }
+    }
+}
